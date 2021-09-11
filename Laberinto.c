f17e8820a8fb1160fe89ac50ab34639a40d25f7e
TomasCastro <tomascastrorojas18@gmail.com>
2021-09-11 18:00:14 -0300

    Archivos recuperados

---
+++ Laberinto.c
@@ -0,0 +1,180 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+//Largo del buffer donde almacena la informacion innecesaria
+#define LARGO_BUFFER 100
+/*determinarInicializacion: FILE* -> Bool
+Recibe un archivo con el formato de entrada, si la cantidad de obstaculos random es mayor
+a la mitad de casillas disponibles devuelve 1, sino 0*/
+int determinarInicializacion(FILE *archivo){
+    char buffer[LARGO_BUFFER];
+    int dimension,obsFijos=0,obsRandom;
+    fscanf(archivo,"%s\n%d\n",buffer,&dimension);
+    fgets(buffer,LARGO_BUFFER,archivo);
+    while(fgetc(archivo)=='('){
+        fgets(buffer,LARGO_BUFFER,archivo);
+        obsFijos++;
+    }
+    fgets(buffer,LARGO_BUFFER,archivo);
+    fscanf(archivo,"%d\n",&obsRandom);
+    return (obsRandom>(((dimension*dimension)-obsFijos-2)/2));
+}
+/*inicializarLaberinto: char** int int
+Recibe un laberinto, su dimension y la condicion de seteo.
+Si la condicion es 1, inicializa todas las casillas en '1'.Sino, en '0'*/
+void inicializarLaberinto (char **laberinto,int dimension,int condicion){
+    char caracter = condicion +'0';
+    for(int i=0;i<dimension;++i){
+        laberinto[i]=malloc(sizeof(char)*(dimension+1));
+        laberinto[0][i]= caracter;
+    }
+    laberinto[0][dimension]='\0';
+    for(int i=1; i<dimension;++i){
+        strcpy(laberinto[i],laberinto[0]);
+    }
+}
+/*inicializarLaberinto: char** int
+Recibe una matriz char y su tamaño, libera el espacio de memoria de cada
+puntero en la matriz, luego libera la matriz*/
+void liberarMemoria (char **array,int dimension){
+    for(int i=0;i<dimension;++i){
+        free(array[i]);
+    }
+    free(array);
+}
+/*verificar: int int int char** char -> bool
+Recibe una numero de fila, un numero de columna, un laberinto, su tamaño y
+un caracter que representa espacio no ocupado por otro objeto.
+Devuelve 1 si la posicion esta dentro del laberinto y no es una posicion ya ocupada,
+en caso contrario 0*/
+int verificar(int posX, int posY,int dimension, char **laberinto,char caracter){
+    return (posX>0 && posY>0)&&(posX<=dimension&&posY<=dimension)&&(laberinto[posX-1][posY-1]==caracter);
+}
+/*obstaculosRandom: char** int int int int char*
+Recibe un laberinto, su dimension, la condicion de seteo, la cantidad de obstaculos random a poner,
+la cantidad de objetos fijos puestos y un numero en un array char para la seedrand.
+Si la condicion es 1, coloca '0' (caminos libres)
+Si la condicion es 0, coloca '1 (paredes)'*/
+void obstaculosRandom(char **laberinto,int dimension,int condicion,int cantObsRandom,int cantObsFijos,char *randomSeed){
+    int filaRandom,columnaRandom;
+    char posDisponible = condicion +'0';
+    srand(atoi(randomSeed));
+    if(condicion){
+        int caminosPuestos=0,caminos_a_poner=((dimension*dimension)-cantObsFijos-2)-cantObsRandom;
+        while(caminosPuestos<caminos_a_poner){
+            filaRandom = rand()%dimension;
+            columnaRandom = rand()%dimension;
+            while(laberinto[filaRandom][columnaRandom]!= posDisponible){
+                filaRandom = rand()%dimension;
+                columnaRandom = rand()%dimension;
+            }
+            laberinto[filaRandom][columnaRandom]= '0';
+            caminosPuestos++;
+        }
+    }
+    else{
+        int obsRandomPuestos=0;
+        while(obsRandomPuestos<cantObsRandom){
+            filaRandom = rand()%dimension;
+            columnaRandom = rand()%dimension;
+            while(laberinto[filaRandom][columnaRandom]!= posDisponible){
+                filaRandom = rand()%dimension;
+                columnaRandom = rand()%dimension;
+            }
+            laberinto[filaRandom][columnaRandom]= '1';
+            obsRandomPuestos++;
+
+        }
+    }
+}
+/*layoutLaberinto: FILE* char** int int char* -> bool
+Recibe un archivo, un laberinto ya inicializado, la condicion en la que se inicializo,
+su dimension y un numero en un array char.
+A medida que lee el archivo, coloca los obstaculos fijos, que segun la condicion colocara '1' o '2' respectivamente,
+la salida y el objetivo. Siempre verificando que los datos en la entrada sean validos.
+En caso de no serlos, devuelve 0. Sino, 1*/
+int layoutLaberinto (FILE *archivo,char **laberinto,int dimension,int condicion,char *randomSeed){
+    int validez=1,cantObsFijos=0,fila,columna,obsRandom;
+    char caminoLibre = condicion +'0',paredFija = condicion + '1',buffer[LARGO_BUFFER];
+
+    fgets(buffer,LARGO_BUFFER,archivo);
+    while(fgetc(archivo) == '('&&validez==1){
+        fscanf(archivo,"%d,%d)\n",&fila,&columna);
+        if(verificar(fila,columna,dimension,laberinto,caminoLibre)){
+            laberinto[fila-1][columna-1]= paredFija;
+            cantObsFijos++;
+        }
+        else validez=0;
+    }
+    if (validez){
+        fgets(buffer,LARGO_BUFFER,archivo);
+        fscanf(archivo,"%d\n",&obsRandom);
+        if(obsRandom>((dimension*dimension)-cantObsFijos-2))validez=0;
+        else{
+            fgets(buffer,LARGO_BUFFER,archivo);
+            fscanf(archivo,"(%d,%d)\n",&fila,&columna);
+            if(verificar(fila,columna,dimension,laberinto,caminoLibre)){
+                laberinto[fila-1][columna-1]='I';
+                }
+            else validez=0;
+            if(validez){
+                fgets(buffer,LARGO_BUFFER,archivo);
+                fscanf(archivo,"(%d,%d)\n",&fila,&columna);
+                if(verificar(fila,columna,dimension,laberinto,caminoLibre)){
+                laberinto[fila-1][columna-1]='X';}
+                else validez=0;
+                if(validez){
+                    obstaculosRandom(laberinto,dimension,condicion,obsRandom,cantObsFijos,randomSeed);
+                    if(condicion){
+                        int paredesCambiadas=0;
+                        for(int i=0;i<dimension&&paredesCambiadas<cantObsFijos;i++){
+                            for(int j=0;j<dimension&&paredesCambiadas<cantObsFijos;j++){
+                                if(laberinto[i][j]=='2'){
+                                    laberinto[i][j]='1';
+                                    paredesCambiadas++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+    fclose(archivo);
+    return validez;
+}
+/*escritura: char** int char*
+Recibe un laberinto, su dimension y el nombre del archivo de salida.
+Escribe en el archivo el laberinto fila por fila*/
+void escritura (char **laberinto,int dimension,char fileSalida[]){
+    FILE *archivoSalida = fopen(fileSalida,"w");
+    for(int i=0;i<dimension;++i){
+        fprintf(archivoSalida,"%s\n",laberinto[i]);
+    }
+    fclose(archivoSalida);
+}
+int main (int Argc,char *argumentos[]){
+    FILE *Entrada = fopen(argumentos[1],"r");
+    int condicion;
+    condicion = determinarInicializacion(Entrada);
+    rewind(Entrada);
+    int dimension;
+    char buffer[LARGO_BUFFER];
+
+    fgets(buffer,LARGO_BUFFER,Entrada);
+    fscanf(Entrada,"%d\n",&dimension);
+    char **laberinto=(char**)malloc(sizeof(char*)*dimension);
+    inicializarLaberinto(laberinto,dimension,condicion);
+    if(layoutLaberinto(Entrada,laberinto,dimension,condicion,argumentos[3])){
+        escritura(laberinto,dimension,argumentos[2]);
+        liberarMemoria(laberinto,dimension);
+    }
+    else{
+        printf("La entrada no es valida\n");
+        liberarMemoria(laberinto,dimension);
+    }
+
+    return 0;
+}
---
+++ ResolverLaberinto.py
@@ -0,0 +1,126 @@
+import subprocess
+from math import sqrt
+import random
+""" Diseño de datos:
+    Representaremos un laberinto como una lista de listas (una matriz) donde cada lista representa
+    una fila y la posicion en esa lista, la columna. Lo que se encuentra en cada casilla del laberinto se representa
+    a traves de un caracter string.
+
+    Para representar la ubicacion, posicion, coordenadas de una casilla o nodo del laberinto usaremos una dupla (int,int)
+    donde el primer elemento representa la fila-1 y el segundo la columna-1. Tambien cuando nos referimos a nodo, objetivo
+    o inicio, en realidad nos referimos a sus coordenadas.
+
+    El programa comienza generando un numero random que sera la seedrand para el programa en C, luego ejecuta el archivo
+    compilado de C pasando como argumentos el archivo de entrada, el nombre del texto de salida y la seedrand.
+    
+    En caso que el laberinto no tenga solucion, se generan laberintos hasta que alguno tenga solucion."""
+
+# encontrar: List[List[string]] int -> Tupla(Tupla(int))
+# Recibe un laberinto y su tamaño, devuelve en forma de tupla, las tuplas con las coordenadas
+# de la salida y el objetivo respectivamente
+def encontrar(laberinto,dimension):
+    fila = 0
+    columna = 0
+    encontrado = 0
+    while(fila<dimension and encontrado<2):
+        while(columna<dimension and encontrado<2):
+            if(laberinto[fila][columna]=="I"):
+                inicio = (fila,columna)
+                encontrado +=1
+            if(laberinto[fila][columna]=="X"):
+                final = (fila,columna)
+                encontrado +=1
+            columna+=1
+        columna = 0
+        fila+=1
+    return (inicio,final)
+
+# distancia: Tupla(int) Tupla(int) -> int
+# Dado un nodo del laberinto y el objetivo, devuelve la distancia entre ellos
+def distancia(nodo,objetivo):
+    return sqrt((objetivo[0]-nodo[0])**2 + (objetivo[1]-nodo[1])**2)
+
+# ordenarPorDistancia: List[Tupla(int)] Tupla(int)
+# Dada una lista de nodos y el objetivo, ordena la lista en base a la distancia
+# al objetivo de cada nodo de mayor a menor
+def ordenarPorDistancia(listaNodos,objetivo):
+    listaDistancia = []
+    for nodo in listaNodos:
+        Dist = distancia(nodo,objetivo)
+        listaDistancia.append((nodo[0],nodo[1],Dist))
+        
+    listaNodos.clear()
+    listaDistancia.sort(key=lambda tupla: tupla[2],reverse=True)
+    for nodo in listaDistancia:
+        listaNodos.append((nodo[0],nodo[1]))
+
+# explorar: List[List[string]] Tupla(int) Tupla(int) int -> List[Tupla(int)]
+# Dado un laberinto, un nodo, el objetivo y el tamaño del laberinto,
+# devuelve una lista con los nodos adyacentes al nodo de entrada 
+# que no sean una pared y no hayan sido visitados ordenados por la distancia al objetivo
+def explorar(laberinto,nodo,objetivo,dimension):
+    DicV = [1,0,-1,0]
+    DicH = [0,1,0,-1]
+    Adyacentes = []
+    for index in range(0,4):
+        X = nodo[0] + DicV[index]
+        Y = nodo[1] + DicH[index]
+        if X>=0 and Y>=0 and X<dimension and Y<dimension and laberinto[X][Y]!="1" and laberinto[X][Y]!="-1":
+            Adyacentes.append((X,Y))
+    ordenarPorDistancia(Adyacentes,objetivo)
+    return Adyacentes
+
+# resolverLaberinto: List[List[string]] Tupla(int) Tupla(int) int -> List[Tupla(int)]
+# Recibe un laberinto, el inicio, el objetivo, y la dimension del laberinto,
+# devuelve una lista de nodos en secuencia que representan una solucion del laberinto
+def resolverLaberinto(laberinto,inicio,objetivo,dimension):
+    stack = []
+    stack.append([inicio])
+    Solucion = []
+    llegarObjetivo = False
+    while(not llegarObjetivo and stack):
+        path = stack.pop(-1)
+        nodo = path[-1]
+        if laberinto[nodo[0]][nodo[1]]!="-1":
+            laberinto[nodo[0]][nodo[1]]="-1"
+            vecinos = explorar(laberinto,nodo,objetivo,dimension)
+            for nodoVecino in vecinos:
+                newPath = path + [nodoVecino]
+                stack.append(newPath)
+                if laberinto[nodoVecino[0]][nodoVecino[1]]=="X":
+                    llegarObjetivo = True
+                    Solucion = newPath
+    return Solucion
+
+# imprimirSolucion: List[Tupla(int)]
+# Recibe una solucion del laberinto, escribe la secuencia de nodos en un archivo
+def imprimirSolucion(Solucion,archivoSolucion):
+    salida = open(archivoSolucion,"w")
+    for pasos in Solucion:
+        salida.write("({0},{1})\n".format(pasos[0]+1,pasos[1]+1))
+    salida.close()
+
+def main():
+    archivoEntradaC = "entrada.txt"
+    archivoLaberinto = "salida.txt"
+    archivoSalida = "solucion.txt"
+    ejecutableDeC = "./a.exe"
+    randomSeed = str(random.randrange(1000000000))
+    ejecutar = subprocess.run([ejecutableDeC,archivoEntradaC,archivoLaberinto,randomSeed])
+    #Pregunta si se genero la salida, en caso que sea False significa que la entrada no era valida
+    if(ejecutar.returncode == 0):
+        Entrada = open(ejecutar.args[2],"r")
+        laberinto =list(map(lambda linea:list(linea.strip()),Entrada.readlines()))
+        Entrada.close()
+        dimension = len(laberinto)
+        inicio_fin = encontrar(laberinto,dimension)
+        recorrido = resolverLaberinto(laberinto,inicio_fin[0],inicio_fin[1],dimension)
+        while(recorrido == []):
+            randomSeed = str(random.randrange(1000000000))
+            ejecutar = subprocess.run([ejecutableDeC,archivoEntradaC,archivoLaberinto,randomSeed])
+            Entrada = open(ejecutar.args[2],"r")
+            laberinto =list(map(lambda linea:list(linea.strip()),Entrada.readlines()))
+            Entrada.close()
+            recorrido = resolverLaberinto(laberinto,inicio_fin[0],inicio_fin[1],dimension)
+        imprimirSolucion(recorrido,archivoSalida)
+main()
